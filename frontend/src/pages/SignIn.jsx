a2ec7dfcd08b4130b2d0eb5bde6375046b13040e
MohitGupta <krishgupta1630@gmail.com>
2024-02-17 15:23:43 +0530

    adding signIn and signUp page ui

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-6 max-w-3xl">
+        {/* Left */}
+        <div className="flex-1">
+          <Link to={"/"} className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-pink-500 via-indigo-500 to-purple-500 rounded-lg text-white">
+              Insight
+            </span>
+            Hub
+          </Link>
+          <p className="mt-5 text-sm md:text-lg">
+            Welcome to InsighHub! Join our community of avid readers and
+            passionate writers.
+          </p>
+        </div>
+
+        {/* Right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Email" />
+              <TextInput type="email" placeholder="nam@gmail.com" id="email" />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput type="password" placeholder="Password" id="password" />
+            </div>
+            <Button type="submit" gradientDuoTone={"purpleToPink"}>
+              Sign In
+            </Button>
+          </form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account?</span>
+            <Link to={"/sign-up"} className="underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
